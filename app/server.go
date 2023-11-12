59e2b61704ded2d4cf57ca7d0229e378cef7fe5e
Sanyam Khurana <sanyam.khurana01@gmail.com>
2023-11-12 06:01:40 +0530

    feat: Parse & reply to echo command

--- app/server.go
+++ app/server.go
@@ -6,24 +6,67 @@ import (
 	"io"
 	"net"
 	"os"
+	"strings"
 )
 
+func parseCommand(encodedCommand []byte) (command string, err error) {
+	// TODO: Add validation for correct RESP format
+
+	commandString := string(encodedCommand)
+	if commandString[0] != '*' {
+		return "", errors.New("Invalid command")
+	}
+	args := strings.Split(commandString, "\r\n")
+	for i, arg := range args[2:] {
+		if i%2 == 0 {
+			command += arg + " "
+		}
+	}
+	return command, nil
+}
+
 func handleConnection(conn net.Conn, buffer []byte) {
-	var err error
 	for {
-		_, err = conn.Read(buffer)
+		n, err := conn.Read(buffer)
 		if errors.Is(err, io.EOF) {
-			fmt.Println("Error reading -- EOF reached: ", err.Error())
+			fmt.Println("[INFO] EOF detected: ", err.Error())
 			break
 		}
 		if err != nil {
 			fmt.Println("Error reading: ", err.Error())
 		}
-		_, err = conn.Write([]byte("+PONG\r\n"))
+		respEncodedCommand := buffer[:n]
+		command, err := parseCommand(respEncodedCommand)
 		if err != nil {
-			fmt.Println("Error writing: ", err.Error())
+			fmt.Println("Error parsing command: ", err.Error())
+			break
 		}
+
+		response := processCommand(command)
+		generateResponse(conn, response)
 	}
+}
+
+func generateResponse(conn net.Conn, response string) {
+	_, err := conn.Write([]byte(response))
+	if err != nil {
+		fmt.Println("Error writing: ", err.Error())
+	}
+}
+
+func processCommand(commandString string) (response string) {
+	parsedCommand := strings.Split(commandString, " ")
+	command := strings.ToUpper(parsedCommand[0])
+	args := strings.Trim(strings.Join(parsedCommand[1:], " "), " ")
+	switch command {
+	case "PING":
+		response = "+PONG\r\n"
+	case "ECHO":
+		response = fmt.Sprintf("$%d\r\n%s\r\n", len(args), args)
+	default:
+		response = "-ERR unknown command\r\n"
+	}
+	return response
 }
 
 func main() {
@@ -35,6 +78,7 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
+	defer l.Close()
 
 	buffer := make([]byte, 1024)
 
